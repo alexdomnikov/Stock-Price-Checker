f49be5892c2c9b95c9181d1a68530173efc021e7
Alex Domnikov <179528890+alexdomnikov@users.noreply.github.com>
2025-10-02 16:55:46 -0700

    Updated to-do list

--- main.py
+++ main.py
@@ -5,6 +5,8 @@
 #       Add more interactive chart rather than what you've got now. 
 #       Allow users to change their password.
 #       Allow users to delete their account. 
+#       Allow users to look up a stock without adding it to their watchlist (i.e., add the graphing functionality to their dashboard)
+#       Allow users to add a stock after looking it up (add this to your watchlist), redirect them to the watchlist.
 
 from project import create_app
 from project import db
