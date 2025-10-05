e7da382309b039be2a213d192f0a437a17b755c9
Alex Domnikov <179528890+alexdomnikov@users.noreply.github.com>
2025-10-04 19:17:47 -0700

    Hid everything behind login screen, added more robust error checking, combined dashboard with price chart lookup page (original index).

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
-# TODO: Check if user hit API rate limits when they access info. 
-#       Create user-friendly API rate limit message.
-#       Cache API responses for charts so we don't burn through rate limits. 
-#       Allow users to click on a stock name from dashboard and get the chart.
-#       Add more interactive chart rather than what you've got now. 
-#       Allow users to change their password.
-#       Allow users to delete their account. 
-#       Allow users to look up a stock without adding it to their watchlist (i.e., add the graphing functionality to their dashboard)
-#       Allow users to add a stock after looking it up (add this to your watchlist), redirect them to the watchlist.
+# TODO: Go through everything line by line and make sure you have robust error checking.
+#
+# Optional changes:
+#       1. Change db from a one-to-many relationship (users have a watchlist, we update watchlists)
+#          to a many-to-many relationship (we have a stock class, watchlists are a relationship). This helps limit API calls.
+#       1. (contd.) If we do this, we need to instead update the watched stocks daily.
+#       2. Allow users to change their password.
+#       3. Allow users to delete their account.
+#       4. Deploy for convenience.
+#       5. Add more interactive chart rather than what you've got now. 
 
 from project import create_app
 from project import db
